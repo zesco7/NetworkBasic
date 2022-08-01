a577721e14c566588fbdc2578ce38146aa6bd84a
hello <hello>
2022-08-01 19:31:40 +0900

    021과제_case1완성

--- NetworkBasic/LottoViewController.swift
+++ NetworkBasic/LottoViewController.swift
@@ -16,6 +16,13 @@ class LottoViewController: UIViewController {
 
     @IBOutlet weak var numberTextField: UITextField!
     
+    @IBOutlet weak var luckyNumber1: UILabel!
+    @IBOutlet weak var luckyNumber2: UILabel!
+    @IBOutlet weak var luckyNumber3: UILabel!
+    @IBOutlet weak var luckyNumber4: UILabel!
+    @IBOutlet weak var luckyNumber5: UILabel!
+    @IBOutlet weak var luckyNumber6: UILabel!
+    @IBOutlet weak var bonusNumber: UILabel!
     
     var lottoPickerView = UIPickerView()
     
@@ -33,8 +40,15 @@ class LottoViewController: UIViewController {
         
     }
     
+    //로또API호출 메서드 생성
     func requestLotto(number: Int) {
         //AF: status code 중 200~299까지 성공이고 300이상 추가하려면 validate에 statusCode 추가해야함.
+        //validate는 상태코드 처리관리함
+        //어디에 요청할지 정해야 되므로 요청대상 url주소를 설정해줘야함.
+        //AF.request(url, method: .get).validate(statusCode: 200..<400)는 알라모 파이어 관련 코드
+        //responseJSON ~~는 responseJSON 관련 코드
+        //접두어 최신버젼 Alamofire -> AF
+        
         let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(number)"
         AF.request(url, method: .get).validate(statusCode: 200..<400).responseJSON { response in
             switch response.result {
@@ -42,13 +56,39 @@ class LottoViewController: UIViewController {
                 let json = JSON(value)
                 print("JSON: \(json)")
                 
-                let bonus = json["bnusNo"].intValue
-                print(bonus)
+                let luckyNumber1 = json["drwtNo1"].stringValue
+                print(luckyNumber1)
+                
+                let luckyNumber2 = json["drwtNo2"].stringValue
+                print(luckyNumber2)
+                
+                let luckyNumber3 = json["drwtNo3"].stringValue
+                print(luckyNumber3)
+                
+                let luckyNumber4 = json["drwtNo4"].stringValue
+                print(luckyNumber4)
+                
+                let luckyNumber5 = json["drwtNo5"].stringValue
+                print(luckyNumber5)
+                
+                let luckyNumber6 = json["drwtNo6"].stringValue
+                print(luckyNumber6)
+                
+                let bonusNumber = json["bnusNo"].stringValue
+                print(bonusNumber)
                 
                 let date = json["drwNoDate"].stringValue
                 print(date)
                 
                 self.numberTextField.text = date
+                self.luckyNumber1.text = luckyNumber1
+                self.luckyNumber2.text = luckyNumber2
+                self.luckyNumber3.text = luckyNumber3
+                self.luckyNumber4.text = luckyNumber4
+                self.luckyNumber5.text = luckyNumber5
+                self.luckyNumber6.text = luckyNumber6
+                self.bonusNumber.text = bonusNumber
+                
                 
             case .failure(let error):
                 print(error)
