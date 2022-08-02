eafd05a84adfa8b14c6e607be76eeda9358b45c1
hello <hello>
2022-08-02 18:41:05 +0900

    주석 추가

--- NetworkBasic/SearchViewController.swift
+++ NetworkBasic/SearchViewController.swift
@@ -17,13 +17,13 @@ import SwiftyJSON
  -. Datasource
  
  1. 왼팔/오른팔
- 2.
  */
 
 /*
 -. 테이블뷰컨트롤러가 아닌 뷰컨트롤러에서 테이블뷰 함수를 사용하려면 테이블뷰 프로토콜을 선언해야 한다.
 -. 테이블뷰 필수함수를 선언해야 한다.
 -. 테이블뷰가 사용할 테이블뷰셀(XIB)을 등록해야 한다.(XIB: Xml Interface Builder (예전에는 NIB))
+-. 뷰컨트롤러 배경색과 테이블뷰 배경색을 맞추려면 테이블뷰 배경색을 .clear로 처리한다.
  */
 
 
@@ -38,12 +38,10 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         searchTableView.delegate = self
         searchTableView.dataSource = self
         searchTableView.register(UINib(nibName: ListTableViewCell.identifier, bundle: nil), forCellReuseIdentifier:  ListTableViewCell.identifier)
-    
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-   
+        return 100
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -52,7 +50,8 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         cell.titleLabel.font = .boldSystemFont(ofSize: 22)
         cell.titleLabel.textAlignment = .center
         cell.titleLabel.text = "HELLO"
-        
+        //cell.backgroundColor = .clear
+    
         return cell
     }
 }
