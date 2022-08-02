7b9ce2626dbaeaaaaeb578f89514ea2d6fa10888
hello <hello>
2022-08-02 20:31:11 +0900

    021_맥주과제 완성

--- NetworkBasic/BeerRecommendationViewController.swift
+++ NetworkBasic/BeerRecommendationViewController.swift
@@ -11,25 +11,27 @@ import Alamofire
 import Kingfisher
 import SwiftyJSON
 
-
+/*
+ -. 맥주정보데이터를 받아올 URL을 상수선언한다.
+ -. 화면에 표시할 정보값을 추려서 json형식으로 받아온다.(배열형식인 경우 표현 유의 : let beerImage = json[0]["image_url"].stringValue)
+ -. 이미지URL문자열을 킹피셔로 URL화하고 이미지뷰에 이미지를 불러온다.
+ -. 랜덤데이터를 호출하도록 버튼클릭 액션을 추가한다.
+ */
 
 class BeerRecommendationViewController: UIViewController {
 
-    
     @IBOutlet weak var beerImage: UIImageView!
     @IBOutlet weak var beerName: UILabel!
     @IBOutlet weak var beerDescription: UITextView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadBeerInfo()
-    
     }
     
     func loadBeerInfo() {
-        let URL = "https://api.punkapi.com/v2/beers/random"
+        let beerInfoUrl = "https://api.punkapi.com/v2/beers/random"
         
         //AF: status code 중 200~299까지 성공이고 300이상 추가하려면 validate에 statusCode 추가해야함.
         //validate는 상태코드 처리관리함
@@ -38,7 +40,7 @@ class BeerRecommendationViewController: UIViewController {
         //responseJSON ~~는 responseJSON 관련 코드
         //접두어 최신버젼 Alamofire -> AF
         
-        AF.request(URL, method: .get).validate(statusCode: 200..<400).responseJSON { response in
+        AF.request(beerInfoUrl, method: .get).validate(statusCode: 200..<400).responseJSON { response in
             
             switch response.result {
             case .success(let value):
@@ -54,8 +56,9 @@ class BeerRecommendationViewController: UIViewController {
                 let beerDescription = json[0]["description"].stringValue
                 print(beerDescription)
                 
-                let imageUrl = url(String: beerImage)
-                self.beerImage.image = beerImage.kf.setImage(with: imageUrl)
+                let imageUrl = URL(string: beerImage)
+                self.beerImage.kf.setImage(with: imageUrl)
+                //self.beerImage.kf.setImage(with: URL(string: beerImage)!) 57,58행을 59행처럼 표현 가능
                 self.beerName.text = beerName
                 self.beerDescription.text = beerDescription
                 
